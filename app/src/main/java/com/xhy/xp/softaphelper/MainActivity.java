d0c28fb8dcd32629b9860c485b9e687a0470e80f
XhyEax <xhyeax@qq.com>
2022-11-18 19:48:31 +0800

    check installed app

--- app/src/main/java/com/xhy/xp/softaphelper/MainActivity.java
+++ app/src/main/java/com/xhy/xp/softaphelper/MainActivity.java
@@ -1,7 +1,11 @@
 package com.xhy.xp.softaphelper;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> pkgNameList = new ArrayList<>();
+        pkgNameList.add("com.android.networkstack.tethering.inprocess");
+        pkgNameList.add("com.android.networkstack.tethering");
+        pkgNameList.add("com.google.android.networkstack.tethering.inprocess");
+        pkgNameList.add("com.google.android.networkstack.tethering");
+
+        StringBuilder sb = new StringBuilder("Installed App (if not found, select android):\n");
+        for (String pkgName: pkgNameList ) {
+            if(isInstalled(pkgName)){
+                sb.append(pkgName);
+            }
+        }
+        TextView textView = findViewById(R.id.sample_text);
+        textView.setText(sb.toString());
     }
+
+    public boolean isInstalled(String pkgName){
+        PackageManager packageManager = this.getApplicationContext().getPackageManager();
+        try {
+            packageManager.getApplicationInfo(pkgName,PackageManager.GET_UNINSTALLED_PACKAGES);
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
+
 }
